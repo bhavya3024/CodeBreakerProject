bfad29e7a04bfbd0bae899d652a19cf516ca6294
bhavya2810 <bhavyadhiman7@gmail.com>
2017-12-19 14:56:47 +0530

    poooosh

--- src/assets/main.js
+++ src/assets/main.js
@@ -16,7 +16,7 @@ function guess() {
 //implement new functions here
 function setHiddenFields(){
  answer.value = Math.floor(Math.random()*10000).toString();
- attempt = 0;
+ attempt.value = 0;
   while(answer.value.length < 4){
       answer.value = '0'+ answer.value;
   }
