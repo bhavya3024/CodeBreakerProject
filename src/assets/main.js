c4c433335a25ee369d7e52e7a050e38bfb92fcc2
bhavya2810 <bhavyadhiman7@gmail.com>
2017-12-19 14:38:00 +0530

    phew

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,6 +1,5 @@
 let answer = document.getElementById('answer');
 let attempt = document.getElementById('attempt');
-let message = document.getElementById('message');
 function guess() {
     let input = document.getElementById('user-guess');
     //add functionality to guess function here
@@ -8,7 +7,7 @@ function guess() {
         attempt++;
     }
     else{
-        setMessage(message);
+        setMessage('Guesses must be exactly 4 characters long.');
         return false;
     }
     if(annswer === ''|| attempt === '')
@@ -24,7 +23,7 @@ function setHiddenFields(){
   }
 }
 function setMessage(message){
-   message.innerHTML = "Guesses must be exactly 4 characters long.";
+   document.getElementById("message").innerHTML = message;
 }
 function validateInput(input){
    if(input.length === 4){
