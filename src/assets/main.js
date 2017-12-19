daf46be472eb75fa3c70663769a7229d4a6a9ebf
bhavya2810 <bhavyadhiman7@gmail.com>
2017-12-19 15:42:13 +0530

    poosh

--- src/assets/main.js
+++ src/assets/main.js
@@ -48,6 +48,7 @@ function validateInput(input){
 }
 function getResults(input){
     let html =  `<div class='row'><span class = 'col-md-6'>${input}</span><div class='col-md-6'>`;
+    let correct = 0;
     for(let i =0;i<input.length;i++){
         if(input.charAt(i) === answer.value.charAt(i)){
             html += `<span class='glyphicon glyphicon-ok'></span>`;
